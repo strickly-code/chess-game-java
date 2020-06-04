ebe2eca7a836d9ef08d4ca79fdc0982850ffbbcf
Thiago Alves <tiga_alves@hotmail.com>
2020-06-04 10:42:02 -0300

    Project created

---
+++ chess-system/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+/**
+ *
+ * @author Thiago
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    
+}
